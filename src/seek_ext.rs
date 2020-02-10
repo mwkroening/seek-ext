55c8eb780d905c1651740f7fcae8738cc4b80fb5
Martin Kröning <m.kroening@hotmail.de>
2020-02-10 12:16:22 +0100

    Rename current_position to stream_position
    This aligns the name with the unstable "seek_convenience" feature.

--- src/seek_ext.rs
+++ src/seek_ext.rs
@@ -34,12 +34,12 @@ pub trait SeekExt: Seek {
     /// let pos_before = c.seek(SeekFrom::Current(4))?;
     ///
     /// assert_eq!(c.stream_len()?, 6);
-    /// assert_eq!(c.current_position()?, pos_before);
+    /// assert_eq!(c.stream_position()?, pos_before);
     /// # Ok(())
     /// # }
     /// ```
     fn stream_len(&mut self) -> Result<u64> {
-        let old_pos = self.current_position()?;
+        let old_pos = self.stream_position()?;
         let len = self.seek(SeekFrom::End(0))?;
         self.seek(SeekFrom::Start(old_pos))?;
         Ok(len)
@@ -60,14 +60,14 @@ pub trait SeekExt: Seek {
     /// let mut c = Cursor::new(vec![0; 6]);
     ///
     /// c.seek(SeekFrom::Current(4))?;
-    /// assert_eq!(c.current_position()?, 4);
+    /// assert_eq!(c.stream_position()?, 4);
     ///
     /// c.seek(SeekFrom::Current(-3))?;
-    /// assert_eq!(c.current_position()?, 1);
+    /// assert_eq!(c.stream_position()?, 1);
     /// # Ok(())
     /// # }
     /// ```
-    fn current_position(&mut self) -> Result<u64> {
+    fn stream_position(&mut self) -> Result<u64> {
         self.seek(SeekFrom::Current(0))
     }
 }
@@ -86,39 +86,39 @@ mod tests {
         assert_eq!(c.stream_len().unwrap(), 15);
 
         c.seek(SeekFrom::End(0)).unwrap();
-        let old_pos = c.current_position().unwrap();
+        let old_pos = c.stream_position().unwrap();
         assert_eq!(c.stream_len().unwrap(), 15);
-        assert_eq!(c.current_position().unwrap(), old_pos);
+        assert_eq!(c.stream_position().unwrap(), old_pos);
 
         c.seek(SeekFrom::Start(7)).unwrap();
         c.seek(SeekFrom::Current(2)).unwrap();
-        let old_pos = c.current_position().unwrap();
+        let old_pos = c.stream_position().unwrap();
         assert_eq!(c.stream_len().unwrap(), 15);
-        assert_eq!(c.current_position().unwrap(), old_pos);
+        assert_eq!(c.stream_position().unwrap(), old_pos);
     }
 
     #[test]
-    fn current_position() {
+    fn stream_position() {
         // All `asserts` are duplicated here to make sure the method does not
         // change anything about the seek state.
         let mut c = Cursor::new(vec![0; 15]);
-        assert_eq!(c.current_position().unwrap(), 0);
-        assert_eq!(c.current_position().unwrap(), 0);
+        assert_eq!(c.stream_position().unwrap(), 0);
+        assert_eq!(c.stream_position().unwrap(), 0);
 
         c.seek(SeekFrom::End(0)).unwrap();
-        assert_eq!(c.current_position().unwrap(), 15);
-        assert_eq!(c.current_position().unwrap(), 15);
+        assert_eq!(c.stream_position().unwrap(), 15);
+        assert_eq!(c.stream_position().unwrap(), 15);
 
 
         c.seek(SeekFrom::Start(7)).unwrap();
         c.seek(SeekFrom::Current(2)).unwrap();
-        assert_eq!(c.current_position().unwrap(), 9);
-        assert_eq!(c.current_position().unwrap(), 9);
+        assert_eq!(c.stream_position().unwrap(), 9);
+        assert_eq!(c.stream_position().unwrap(), 9);
 
         c.seek(SeekFrom::End(-3)).unwrap();
         c.seek(SeekFrom::Current(1)).unwrap();
         c.seek(SeekFrom::Current(-5)).unwrap();
-        assert_eq!(c.current_position().unwrap(), 8);
-        assert_eq!(c.current_position().unwrap(), 8);
+        assert_eq!(c.stream_position().unwrap(), 8);
+        assert_eq!(c.stream_position().unwrap(), 8);
     }
 }
